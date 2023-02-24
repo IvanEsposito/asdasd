276e8c90fc29cd73df89632951c72969574cee64
IvanEsposito <ivanesposito@live.com.ar>
2023-02-24 20:57:11 -0300

    Subida archivos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer.jsx";
-import ProductCard from "./Components/ProductCard/ProductCard.jsx";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ItemListContainer greeting={"ivan"}/>
-      <ProductCard title={"Producto 1"} price={200} isRed={false}/>
-      <ProductCard title={"Producto 2"} price={300} isRed={true}/>
       <Footer />
     </div>
   );
